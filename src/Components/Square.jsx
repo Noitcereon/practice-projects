4bc931401134e9b02572aaa6487c5d00b91235a3
Thomas 'Noit' A <tba@live.dk>
2022-04-20 09:38:00 +0200

    refactor: square now uses board to handle state

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 class Square extends React.Component {
   render() {
-      return(
-          <>
-            <button className="square">{this.props.value}</button>
-          </>
-      );
+    return (
+      <>
+        <button
+          className="square"
+          onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
+        </button>
+      </>
+    );
   }
 }
 
