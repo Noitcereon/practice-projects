6c505144dae724efdbdb65f5f975b88d9b0cbfd7
Thomas 'Noit' A <tba@live.dk>
2022-04-20 09:37:17 +0200

    styling: add styling from tutorial + center main

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from 'react'
-import Game from './Components/Game';
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import Game from "./Components/Game";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>React Tutorial App</h1>
-      <Game/>
+      <div className="wrapper">
+        <main>
+          <h1>React Tutorial App</h1>
+          <Game />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
