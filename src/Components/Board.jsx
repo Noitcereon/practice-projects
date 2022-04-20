59a87737b0663e3c97366e421689dd020c25e044
Thomas 'Noit' A <tba@live.dk>
2022-04-20 10:12:05 +0200

    feat: implement 'O' placement and winning

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -6,12 +6,21 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   handleClick(squaresIndex) {
     const squares = this.state.squares.slice();
-    squares[squaresIndex] = "X";
-    this.setState({ squares: squares });
+    // ignore handleClick if there is a winner or the square is already filled
+    if(this.calculateWinner(squares) || squares[squaresIndex]){
+      return; 
+    }
+    squares[squaresIndex] = this.state.xIsNext ? "X" : "O";
+
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   renderSquare(squaresIndex) {
     return (
@@ -21,8 +30,12 @@ class Board extends React.Component {
       ></Square>
     );
   }
+  
+
   render() {
-    const status = "Next player: X";
+    const winner = this.calculateWinner(this.state.squares);
+    let status = `Next player is: ${this.state.xIsNext ? "X" : "O"}`;
+    if(winner) status = `Winner: ${winner}`;
 
     return (
       <div>
@@ -44,6 +57,30 @@ class Board extends React.Component {
         </div>
       </div>
     );
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a]; // return winner
+      }
+    }
+    return null; // no one has won
   }
 }
 
