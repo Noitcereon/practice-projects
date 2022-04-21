c7dd1a959db68acd2cabfecbe5a1e6a790786eec
Thomas 'Noit' A <tba@live.dk>
2022-04-21 15:46:02 +0200

    feat: implement game move history (not visible)

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,45 +1,22 @@
 import React from "react";
 import Square from "./Square";
+import calculateWinner from "../Misc/calculateWinner";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-  handleClick(squaresIndex) {
-    const squares = this.state.squares.slice(); // create a copy of the array with slice (to avoid mutating state)
-    // ignore handleClick if there is a winner or the square is already filled
-    if(this.calculateWinner(squares) || squares[squaresIndex]){
-      return; 
-    }
-    squares[squaresIndex] = this.state.xIsNext ? "X" : "O";
-
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
+  
+  
   renderSquare(squaresIndex) {
     return (
       <Square
-        value={this.state.squares[squaresIndex]}
-        onClick={() => this.handleClick(squaresIndex)}
+        value={this.props.squares[squaresIndex]}
+        onClick={() => this.props.onClick(squaresIndex)}
       ></Square>
     );
   }
   
-
   render() {
-    const winner = this.calculateWinner(this.state.squares);
-    let status = `Next player is: ${this.state.xIsNext ? "X" : "O"}`;
-    if(winner) status = `Winner: ${winner}`;
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -57,30 +34,6 @@ class Board extends React.Component {
         </div>
       </div>
     );
-  }
-
-  calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a]; // return winner
-      }
-    }
-    return null; // no one has won
   }
 }
 
--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,15 +1,52 @@
 import React from "react";
+import calculateWinner from "../Misc/calculateWinner";
 import Board from "./Board";
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
+  handleClick(squaresIndex) {
+    const history = this.state.history;
+    const currentBoard = this.state.history[history.length - 1];
+    const squares = currentBoard.squares.slice();
+    // // ignore handleClick if there is a winner or the square is already filled
+    if (calculateWinner(squares) != null || squares[squaresIndex]) {
+      return;
+    }
+    squares[squaresIndex] = this.state.xIsNext ? "X" : "O";
+
+    this.setState({
+      history: history.concat([{squares}]),
+      xIsNext: !this.state.xIsNext,
+    });
+    console.log("history",history);
+  }
   render() {
+    const history = this.state.history;
+    const currentBoard = this.state.history[history.length - 1];
+    const squares = currentBoard.squares;
+    const winner = calculateWinner(squares);
+    let status = `Next player is: ${this.state.xIsNext ? "X" : "O"}`;
+    if (winner) status = `Winner: ${winner}`;
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={squares}
+            onClick={(squaresIndex) => this.handleClick(squaresIndex)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
