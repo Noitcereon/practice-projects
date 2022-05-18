6bb6c04588b654838a8dc504d1fdc32a64acf711
Thomas 'Noit' A <tba@live.dk>
2022-05-18 13:12:06 +0200

    Feat: Add very simple UUID validation
    To ensure a valid UUID is inserted.
    
    Signed-off-by: Thomas 'Noit' A <tba@live.dk>

--- src/main/java/noitcereon/mydemojavaapi/models/Movie.java
+++ src/main/java/noitcereon/mydemojavaapi/models/Movie.java
@@ -39,7 +39,12 @@ public class Movie {
 
     public void setUuid(String uuid) {
         // TODO: Add UUID validation
-        this.uuid = uuid;
+        if(uuid.length() == 36){
+            this.uuid = uuid;
+        }
+        else{
+            this.uuid = UUID.randomUUID().toString();
+        }
     }
 
     public String getTitle() {
