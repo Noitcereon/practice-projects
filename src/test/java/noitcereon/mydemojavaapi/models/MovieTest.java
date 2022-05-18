565218f1e1961b1641c3d6c4d3baaf8de01e73d4
Thomas 'Noit' A <tba@live.dk>
2022-05-18 11:43:57 +0200

    Test: Setup Movie test class with 1 implemented test
    Signed-off-by: Thomas 'Noit' A <tba@live.dk>

---
+++ src/test/java/noitcereon/mydemojavaapi/models/MovieTest.java
@@ -0,0 +1,57 @@
+package noitcereon.mydemojavaapi.models;
+
+import org.hibernate.id.GUIDGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.rmi.server.UID;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieTest {
+    // Given-When-Then test naming (Behaviour Driven Testing)
+
+    private Movie _movie;
+
+    @BeforeEach
+    void setUp() {
+        _movie = new Movie();
+    }
+
+    @Test
+    void given_validInput_when_usingFullConstructor_then_movieObjectIsCreatedWithGivenInput() {
+        UUID id = UUID.randomUUID();
+        String title = "My Movie";
+        int releaseYear = 2022;
+        int durationInMinutes = 98;
+
+        Movie movie = new Movie(id.toString(), title, releaseYear, durationInMinutes);
+
+        assertEquals(id.toString(), movie.getUuid());
+        assertEquals(title, movie.getTitle());
+        assertEquals(releaseYear, movie.getReleaseYear().get(Calendar.YEAR));
+        assertEquals(durationInMinutes, movie.getDuration().toMinutes());
+    }
+
+    @Test
+    void given_invalidUidInput_when_settingUid_then_uidIsNotSet() {
+
+    }
+
+    @Test
+    void given_invalidYearInput_when_settingReleaseYear_then_yearIsNotSet() {
+
+    }
+
+    @Test
+    void given_invalidDuration_when_settingMovieDuration_then_durationIsNotSet() {
+
+    }
+}
