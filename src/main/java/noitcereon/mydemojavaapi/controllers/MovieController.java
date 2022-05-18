162ac0dc1d9df887b7b686d8fa166058084bd1a8
Thomas 'Noit' A <tba@live.dk>
2022-05-18 16:05:50 +0200

    Refactor(MovieController): Add 'movies' to Controller's RequestMapping
    To follow DRY principle.
    
    Signed-off-by: Thomas 'Noit' A <tba@live.dk>

--- src/main/java/noitcereon/mydemojavaapi/controllers/MovieController.java
+++ src/main/java/noitcereon/mydemojavaapi/controllers/MovieController.java
@@ -1,15 +1,18 @@
 package noitcereon.mydemojavaapi.controllers;
 
+import noitcereon.mydemojavaapi.models.Actor;
 import noitcereon.mydemojavaapi.models.Movie;
 import noitcereon.mydemojavaapi.repositories.IMovieRepository;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/movies")
 public class MovieController {
     private final IMovieRepository movieRepo;
 
@@ -17,14 +20,18 @@ public class MovieController {
         this.movieRepo = movieRepo;
     }
 
-    @GetMapping("movies")
+    @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return ResponseEntity.ok(movieRepo.findAll());
     }
-    @PostMapping("movies")
+    @PostMapping
     public ResponseEntity<Movie> createMovie(@RequestBody Movie movie){
         movie.setUuid(UUID.randomUUID().toString());
         Movie savedMovie = movieRepo.save(movie);
         return ResponseEntity.ok(savedMovie);
+    }
+    @PatchMapping("/{movieId}/actors")
+    public ResponseEntity<Actor> addActors(@PathVariable String movieId, ArrayList<String> actorIds){
+        return null;
     }
 }
