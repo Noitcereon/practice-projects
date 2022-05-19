905c82edbd96c79a23238e5afd964b460ea045ad
Thomas 'Noit' A <tba@live.dk>
2022-05-19 15:18:06 +0200

    Feat: Add basic CRUD for movies
    Signed-off-by: Thomas 'Noit' A <tba@live.dk>

--- src/main/java/noitcereon/mydemojavaapi/controllers/MovieController.java
+++ src/main/java/noitcereon/mydemojavaapi/controllers/MovieController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -20,18 +21,49 @@ public class MovieController {
         this.movieRepo = movieRepo;
     }
 
-    @GetMapping
-    public ResponseEntity<List<Movie>> getAllMovies() {
-        return ResponseEntity.ok(movieRepo.findAll());
-    }
     @PostMapping
-    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie){
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
         movie.setUuid(UUID.randomUUID().toString());
         Movie savedMovie = movieRepo.save(movie);
         return ResponseEntity.ok(savedMovie);
     }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        // TODO: use getAll that does not contain deleted movies.
+        return ResponseEntity.ok(movieRepo.findAll());
+    }
+
+    @GetMapping("{movieId}")
+    public ResponseEntity<Movie> getById(@PathVariable String movieId) {
+        var optionalMovie = movieRepo.findById(movieId);
+        if (optionalMovie.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(optionalMovie.get());
+    }
+
     @PatchMapping("/{movieId}/actors")
-    public ResponseEntity<Actor> addActors(@PathVariable String movieId, ArrayList<String> actorIds){
+    public ResponseEntity<Actor> addActors(@PathVariable String movieId, ArrayList<String> actorIds) {
         return null;
+    }
+
+    @PutMapping("{movieId}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable String movieId, @RequestBody Movie updatedMovie) {
+        updatedMovie.setUuid(movieId);
+        Movie savedObject = movieRepo.save(updatedMovie);
+        return ResponseEntity.ok(savedObject);
+    }
+
+    @DeleteMapping("{movieId}")
+    public ResponseEntity<Movie> disableMovie(@PathVariable String movieId) {
+        Optional<Movie> movieOptional = movieRepo.findById(movieId);
+        if (movieOptional.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        Movie movieToDisable = movieOptional.get();
+        movieToDisable.setDeleted(true);
+        Movie savedObject = movieRepo.save(movieToDisable);
+        return ResponseEntity.ok(savedObject);
     }
 }
