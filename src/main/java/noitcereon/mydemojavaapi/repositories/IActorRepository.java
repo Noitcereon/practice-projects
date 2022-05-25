9e45ec64e275bd8daf0fed8ff23aca9299ffc5df
Thomas 'Noit' A <tba@live.dk>
2022-05-25 16:23:18 +0200

    Fix: Update ActorsFindById to no longer be N+1 effectiveness
    Signed-off-by: Thomas 'Noit' A <tba@live.dk>

--- src/main/java/noitcereon/mydemojavaapi/repositories/IActorRepository.java
+++ src/main/java/noitcereon/mydemojavaapi/repositories/IActorRepository.java
@@ -2,10 +2,19 @@ package noitcereon.mydemojavaapi.repositories;
 
 import noitcereon.mydemojavaapi.models.entities.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
 
 public interface IActorRepository extends JpaRepository<Actor, String> {
     Set<Actor> findAllByIsDeletedIsFalse();
-    Actor findByUuidAndIsDeletedIsFalse(String uuid);
+
+
+    // When Query is there, it overwrites the "derrived" query from the method name.
+    // Sub-queries (a.uuid, a.firstName...) are not available in JPQL, but can be used in native.
+    @Query(value = "SELECT a FROM Actor a" +
+            " JOIN FETCH a.movies WHERE a.uuid = :uuid AND a.isDeleted = false")
+    Actor findByUuidAndIsDeletedIsFalse(@Param("uuid") String uuid);
+
 }
