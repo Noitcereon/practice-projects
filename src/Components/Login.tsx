a2cac66987fa1bc0b65fc937c355319ed7635088
Thomas 'Noit' A <tba@live.dk>
2022-06-08 12:24:40 +0200

    Replace isLoggedIn with keycloak.authenticated

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -3,21 +3,18 @@ import { LoginContext } from "../Context/LoginContext";
 
 export default function Login() {
   const keycloak = useContext(LoginContext);
-  const isLoggedIn: boolean = keycloak.subject !== undefined;
-  console.log(keycloak.subject, "keycloak subject");
-  console.log(isLoggedIn);
   return (
     <section>
       <button
         className={`bg-blue-600 rounded-lg px-5 py-1 
-        ${isLoggedIn ? "hidden" : ""}`}
+        ${keycloak.authenticated ? "hidden" : ""}`}
         onClick={() => keycloak.login()}
       >
         Login
       </button>
       <button
         className={`bg-blue-600 rounded-lg px-5 py-1 
-        ${isLoggedIn ? "" : "hidden"}`}
+        ${keycloak.authenticated ? "" : "hidden"}`}
         onClick={() => keycloak.logout()}
       >
         Logout
