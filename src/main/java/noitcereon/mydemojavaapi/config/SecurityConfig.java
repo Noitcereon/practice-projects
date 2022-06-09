2c0d8844a6df805b8910464ce5cd09d7ab948233
Thomas 'Noit' A <tba@live.dk>
2022-06-09 16:26:31 +0200

    Config(Security): Add basic security config

---
+++ src/main/java/noitcereon/mydemojavaapi/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package noitcereon.mydemojavaapi.config;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // Enable CORS -- this is configured on controllers with @CrossOrigin
+                .cors().and()
+
+                // Sessions will not be used
+                .sessionManagement().disable()
+
+                // Disable Cross Site Request Forgery tokens - They are not needed when there are no sessions.
+                .csrf().disable()
+
+                // Enable security for http requests
+                .authorizeRequests(authorize -> {
+                    authorize
+                            // Specify paths where public access is allowed
+                            // public Swagger paths
+                            .antMatchers("/swagger-ui/**").permitAll()
+                            .antMatchers("/swagger").permitAll()
+                            .antMatchers("/v3/api-docs/**").permitAll()
+
+                            // Other public paths
+                            .antMatchers("/").permitAll()
+                            .antMatchers("/favicon.ico").permitAll()
+
+                            // All remaining paths require authentication
+                            .anyRequest().authenticated();
+                });
+
+    }
+}
