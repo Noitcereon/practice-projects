35a971ebc62ac5a2b3e900dd3068f323c0771392
Thomas 'Noit' A <tba@live.dk>
2022-06-10 15:41:42 +0200

    Add Sql Server Data Tool project with Game table

---
+++ DatabaseSchema/dbo/Tables/Game.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Game]
+(
+	[Id] NVARCHAR(68) NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(250) NOT NULL, 
+    [ReleaseYear] NVARCHAR(4) NOT NULL, 
+    [Price] DECIMAL(18, 2) NULL
+)
