bc3352a44fff4c5f11b814125497fc8c66107db1
Thomas 'Noit' A <tba@live.dk>
2022-06-12 13:47:54 +0200

    Make GamePost inherit from GameBase

--- ModelLib/GamePost.cs
+++ ModelLib/GamePost.cs
@@ -6,22 +6,12 @@ using System.Threading.Tasks;
 
 namespace ModelLib
 {
-    public class GamePost
+    public class GamePost : BaseGame
     {
-        private String _releaseYear = "";
-        private Decimal? _price;
-
-        public String Title { get; set; } = "";
-        public String Description { get; set; } = "";
-        public String ReleaseYear
+        public GamePost() { }
+        public GamePost(String title, String description, String releaseYear, decimal price) : base(title, description, releaseYear, price)
         {
-            get => _releaseYear;
-            set
-            {
-                if (value.Length > 4) throw new ArgumentException("Release year must be 4 or less characters");
-                _releaseYear = value;
-            }
+
         }
-        public decimal? Price { get => _price; set => _price = value; }
     }
 }
