e81e3a67d89987674523432836c8f2ef740568d0
Thomas 'Noit' A <tba@live.dk>
2022-06-14 16:35:30 +0200

    Config(Security): Add OAuth2ResourceServer configuration
    Note: this might not be correct. It is more or less directly
    copied from Greg's SecurityConfig.

--- src/main/java/noitcereon/mydemojavaapi/config/SecurityConfig.java
+++ src/main/java/noitcereon/mydemojavaapi/config/SecurityConfig.java
@@ -3,12 +3,11 @@ package noitcereon.mydemojavaapi.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -42,6 +41,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                             // All remaining paths require authentication
                             .anyRequest().authenticated();
+                })
+                // Configure OAuth2 Resource Server (JWT authentication) so @PreAuthorize can be used
+                .oauth2ResourceServer(oauth2 -> {
+                    // Used to convert JWT to AbstractAuthenticationToken
+                    JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+
+                    // Used to convert JWT 'roles' claim into GrantedAuthorities
+                    JwtGrantedAuthoritiesConverter roleConverter = new JwtGrantedAuthoritiesConverter();
+                    roleConverter.setAuthorityPrefix("ROLE_");
+                    roleConverter.setAuthoritiesClaimName("roles");
+
+                    // Set the jwtConverters JwtGrantedAuthoritiesConverter object
+                    jwtConverter.setJwtGrantedAuthoritiesConverter(roleConverter);
+
+                    // Tell the oauth2 config to use the jwtConverter we just configured.
+                    // This enables JWT authentication and access control from JWT claims
+                    oauth2.jwt().jwtAuthenticationConverter(jwtConverter);
                 });
         logger.trace("Finished Security Configuration");
     }
