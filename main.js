f572caac5671587bc4570d6bad21f440b1a6dd68
Thomas 'Noit' A <tba@live.dk>
2022-06-21 13:42:02 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+/* Table of Contents */
+
+/* 
+--- Common Console Commands ---
+console.log
+console.info
+console.warn
+console.error
+
+--- Less Common Console Commands ---
+console.time / console.timeend
+console.assert
+console.clear
+console.count
+console.table
+
+ */
+
+
