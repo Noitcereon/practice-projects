1bfc2c228ae6c19344c3bd0e34bd6ed4635de2ae
Thomas 'Noit' A <tba@live.dk>
2022-06-22 12:53:04 +0200

    Fix DateAndTime to use private field formatter
    It previously used uncommitted changes from the Human class, which never
    made it into version control.

--- src/Topics/DateAndTime.java
+++ src/Topics/DateAndTime.java
@@ -9,13 +9,16 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateAndTime implements Runnable{
+
+    private final DateTimeFormatter defaultDateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private final DateTimeFormatter defaultDateTimeFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy:HH:mm");
     @Override
     public void run() {
         LocalDate jan1998 = LocalDate.of(1998, 1, 19);
         Human human = new Human("Thomas", jan1998);
-        System.out.println(human.getBirthday().format(human.getDateTimeFormatter()));
+        System.out.println(human.getBirthday().format(defaultDateFormat));
 
         LocalDateTime jan1998At0535 = LocalDateTime.of(jan1998, LocalTime.of(5,35));
-        System.out.println(jan1998At0535.format(DateTimeFormatter.ofPattern("dd/MM/yyyy:HH:mm")));
+        System.out.println(jan1998At0535.format(defaultDateTimeFormat));
     }
 }
