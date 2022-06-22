b1c66fc7c8d5f94336a13f9496d0ae4ccc1df42c
Thomas 'Noit' A <tba@live.dk>
2022-06-22 12:36:21 +0200

    Add basic LocalDate nad LocalDateTime examples

--- src/Topics/DateAndTime.java
+++ src/Topics/DateAndTime.java
@@ -1,10 +1,21 @@
 package Topics;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import Models.Human;
+import com.sun.xml.internal.ws.resources.UtilMessages;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateAndTime implements Runnable{
     @Override
     public void run() {
-        throw new NotImplementedException();
+        LocalDate jan1998 = LocalDate.of(1998, 1, 19);
+        Human human = new Human("Thomas", jan1998);
+        System.out.println(human.getBirthday().format(human.getDateTimeFormatter()));
+
+        LocalDateTime jan1998At0535 = LocalDateTime.of(jan1998, LocalTime.of(5,35));
+        System.out.println(jan1998At0535.format(DateTimeFormatter.ofPattern("dd/MM/yyyy:HH:mm")));
     }
 }
