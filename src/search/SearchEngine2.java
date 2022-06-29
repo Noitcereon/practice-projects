3b054160e60aebfb421a40305a24cd8b96c53b2d
Thomas 'Noit' A <tba@live.dk>
2022-06-29 16:03:58 +0200

    Feat(SearchEngine2):Implement simple strict filter

--- src/search/SearchEngine2.java
+++ src/search/SearchEngine2.java
@@ -1,8 +1,6 @@
 package search;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class SearchEngine2 extends BaseSearchEngine<String>{
@@ -11,8 +9,43 @@ public class SearchEngine2 extends BaseSearchEngine<String>{
     }
 
     @Override
-    public Set<String> search(String input) {
-        String normalizedInput = input.toLowerCase(Locale.ROOT);
-        return data.stream().filter(x -> x.toLowerCase().contains(normalizedInput)).collect(Collectors.toSet());
+    public Set<String> search(String searchQuery) {
+        String normalizedQuery = searchQuery.toLowerCase(Locale.ROOT);
+
+        String[] searchQuerySplitOnQuotes = searchQuery.split("\"");
+        boolean containsStrictSearch = searchQuerySplitOnQuotes.length > 1;
+        if(containsStrictSearch){
+            Collection<String> searchMustContain = findMustContainStrings(searchQuerySplitOnQuotes);
+            return data.stream().filter(dataPiece -> filter(dataPiece, normalizedQuery, searchMustContain)).collect(Collectors.toSet());
+        }
+
+        return data.stream().filter(dataPiece -> filter(dataPiece, normalizedQuery)).collect(Collectors.toSet());
+    }
+
+    private Collection<String> findMustContainStrings(String[] searchQuerySplitOnQuotes) {
+        Collection<String> searchMustContain = new HashSet<>();
+        for (int i = 0; i < searchQuerySplitOnQuotes.length; i++) {
+            boolean isSurroundedByQuotes = (i+1) % 2 == 0;
+            if(isSurroundedByQuotes){
+                searchMustContain.add(searchQuerySplitOnQuotes[i]);
+            }
+        }
+        return searchMustContain;
+    }
+
+    private Boolean filter(String dataPiece, String searchQuery){
+        // Perform loose search
+        System.out.println("Simulating loose filter");
+        // TODO: implement loose search (based on chars, not everything must exist)
+        return true;
+    }
+    private Boolean filter(String dataPiece, String searchQuery, Collection<String> searchMustContain){
+       // Perform strict search
+        String normalizedData = dataPiece.toLowerCase(Locale.ROOT);
+        for (String mustContainThis:searchMustContain) {
+            if(!normalizedData.contains(mustContainThis)) return false;
+        }
+        // TODO: search further with the searchQuery parameter
+        return true;
     }
 }
