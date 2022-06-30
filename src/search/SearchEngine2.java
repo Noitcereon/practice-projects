0ab8f679db4301c9a3375f61b10d884ddce2522e
Thomas 'Noit' A <tba@live.dk>
2022-06-30 13:32:29 +0200

    Feat(Engine2): Implement a simple loose search

--- src/search/SearchEngine2.java
+++ src/search/SearchEngine2.java
@@ -17,9 +17,10 @@ public class SearchEngine2 extends BaseSearchEngine<String> {
         if (containsStrictSearch) {
             String[] searchQuerySplitOnQuotes = searchQuery.split("\"");
             Collection<String> searchMustContain = findMustContainStrings(searchQuerySplitOnQuotes);
+            System.out.println("Strict filter");
             return data.stream().filter(dataPiece -> filter(dataPiece, normalizedQuery, searchMustContain)).collect(Collectors.toSet());
         }
-
+        System.out.println("Loose filter");
         return data.stream().filter(dataPiece -> filter(dataPiece, normalizedQuery)).collect(Collectors.toSet());
     }
 
@@ -36,10 +37,38 @@ public class SearchEngine2 extends BaseSearchEngine<String> {
 
     private Boolean filter(String dataPiece, String searchQuery) {
         // Perform loose search
-        System.out.println("Simulating loose filter");
-        // TODO: implement loose search (based on chars, not everything must exist)
-        // TODO: Write test for loose search
-        return true;
+        // TODO: implement loose search (partial match & count chars is)
+        // lowerCase dataPiece
+        String normalizedData = dataPiece.toLowerCase(Locale.ROOT);
+        // split searchQuery on spaces
+        String[] splitSearchQuery = searchQuery.split("\\s{1,2}"); // 1 or 2 space/linebreak/tabs
+        for (String searchWord : splitSearchQuery) {
+            // if dataPiece contains any of the split pieces
+            if (normalizedData.contains(searchWord)) return true;  // return true
+            if (containsPartialMatch(normalizedData, searchWord)) return true;
+            // if the count of the individual chars matches words (optional)
+            // return true
+        }
+
+        return false;
+    }
+
+    private boolean containsPartialMatch(String normalizedData, String searchWord) {
+        if (searchWord.length() < 3 || normalizedData.length() < searchWord.length()) return false;
+        char[] dataChars = normalizedData.toCharArray();
+        char[] searchWordChars = searchWord.toCharArray();
+
+        // if 3 or more chars (more having higher priority) in a row are the same, return true
+        for (int searchIndex = 0; searchIndex < searchWord.length() - 2; searchIndex++) {
+            for (int dataCharIndex = 0; dataCharIndex < normalizedData.length()-2; dataCharIndex++) {
+                if (dataChars[dataCharIndex] == searchWordChars[searchIndex]
+                        && dataChars[dataCharIndex + 1] == searchWordChars[searchIndex + 1]
+                        && dataChars[dataCharIndex + 2] == searchWordChars[searchIndex + 2]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private Boolean filter(String dataPiece, String searchQuery, Collection<String> searchMustContain) {
