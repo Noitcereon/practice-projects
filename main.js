412d05a8be51bbf98aa4071782499bfbf3ad851d
Thomas 'Noit' A <tba@live.dk>
2022-07-04 15:43:04 +0200

    Add printHelloMessage to main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello `npm publish`")
+export const printHelloMessage = (customMessage="") => {
+    if(customMessage.length === 0){
+        console.log("Hello `npm publish`")
+        return;
+    }
+    console.log(customMessage);
+}
