3b01fa6501a61f14831d36625a82667e182d543f
Thomas 'Noit' A <tba@live.dk>
2022-07-05 15:12:27 +0200

    Test(TimetableGenerator): Add 2 unit tests

--- src/test/java/services/TimetableGeneratorTest.java
+++ src/test/java/services/TimetableGeneratorTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Set;
 
 class TimetableGeneratorTest {
@@ -34,7 +36,45 @@ class TimetableGeneratorTest {
 
     @Test
     void givenValidInput_whenGeneratingTimetable_thenGeneratedTimetableDoesNotHaveOverlaps() {
-        Assertions.fail();
-//        Timetable actual = _generator.generateTimetable();
+        // TODO: rework this, so it takes rooms and teachers into account.
+        Timetable timetable = _generator.generateTimetable(_school, _teachers, _curriculum);
+
+        ArrayList<ScheduleItemInfo> itinerary = new ArrayList<>(timetable.getItinerary());
+        for (int itineraryIndex = 0; itineraryIndex < itinerary.size(); itineraryIndex++) {
+            ScheduleItemInfo item = itinerary.get(itineraryIndex);
+            LocalDateTime start1 = item.getDuration().getStart();
+            LocalDateTime end1 = item.getDuration().getEnd();
+            for (int itineraryIndexCompare = 0; itineraryIndexCompare < itinerary.size(); itineraryIndexCompare++) {
+                ScheduleItemInfo comparisonItem = itinerary.get(itineraryIndexCompare);
+                LocalDateTime start2 = comparisonItem.getDuration().getStart();
+                LocalDateTime end2 = comparisonItem.getDuration().getEnd();
+
+                assertItineraryDoesNotOverlap(itineraryIndex, start1, end1, itineraryIndexCompare, start2, end2);
+            }
+        }
+    }
+    @Test
+    void givenValidInput_whenGeneratingTimetable_thenGeneratedTimetableItineraryIsLargerOrEqualTheAmountOfCurriculumSubjects(){
+        Timetable timetable = _generator.generateTimetable(_school, _teachers, _curriculum);
+        int curriculumSubjects = _curriculum.getAllotedTimePerSubject().size();
+        int itinerarySize = timetable.getItinerary().size();
+        boolean expected = true;
+        boolean actual = itinerarySize >= curriculumSubjects;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    private void assertItineraryDoesNotOverlap(int itineraryIndex, LocalDateTime start1, LocalDateTime end1, int itineraryCompareIndex, LocalDateTime start2, LocalDateTime end2) {
+        int isEarlier = -1;
+        int isSameTime = 0;
+        int isLater = 1;
+        if (start1.compareTo(start2) == isSameTime && itineraryIndex != itineraryCompareIndex) {
+            Assertions.fail("Overlap occurred");
+        } else if (start1.compareTo(start2) == isLater) {
+            Assertions.assertTrue(end1.isAfter(start2));
+            Assertions.assertTrue(end1.isAfter(end2));
+        } else if (start1.compareTo(start2) == isEarlier) {
+            Assertions.assertTrue(end1.isBefore(start2));
+            Assertions.assertTrue(end1.isBefore(end2));
+        }
     }
 }
