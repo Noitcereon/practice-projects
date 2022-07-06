a83ef264c4bee38642c86e016a138b771b939684
Thomas 'Noit' A <tba@live.dk>
2022-07-06 10:56:00 +0200

    Test(TimeRange): Add test for overload method with restrictions

--- src/test/java/models/TimeRangeTest.java
+++ src/test/java/models/TimeRangeTest.java
@@ -1,13 +1,28 @@
 package models;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
 class TimeRangeTest {
+
+
+    private static Collection<DayOfWeek> weekDays;
+
+    @BeforeAll
+    public static void setup(){
+        weekDays = new ArrayList<>();
+        weekDays.add(DayOfWeek.MONDAY);
+        weekDays.add(DayOfWeek.TUESDAY);
+        weekDays.add(DayOfWeek.WEDNESDAY);
+        weekDays.add(DayOfWeek.THURSDAY);
+        weekDays.add(DayOfWeek.FRIDAY);
+    }
 
     @Test
     void givenStartAndEndDateOneDayFromEachOther_whenGettingHoursBetweenStartAndEnd_thenReturn24Hours() {
@@ -17,6 +32,18 @@ class TimeRangeTest {
         TimeRange timeRange = new TimeRange(firstDayOf2022, secondDayOf2022);
 
         int actualHours = timeRange.getHoursBetweenStartAndEnd();
+
+        Assertions.assertEquals(expectedHours, actualHours);
+    }
+    @Test
+    void givenStartAndEndDateOneWeekFromEachOtherWithRestrictions_whenGettingHoursBetweenStartAndEnd_thenReturn30Hours() {
+        LocalDateTime firstDayOf2022 = LocalDateTime.of(2022, 1, 1, 0, 0);
+        LocalDateTime eighthDayOf2022 = firstDayOf2022.plusDays(7);
+        int expectedHours = 30;
+        int hoursUsedPerDay = 6;
+        TimeRange timeRange = new TimeRange(firstDayOf2022, eighthDayOf2022);
+
+        int actualHours = timeRange.getHoursBetweenStartAndEnd(weekDays, hoursUsedPerDay);
 
         Assertions.assertEquals(expectedHours, actualHours);
     }
