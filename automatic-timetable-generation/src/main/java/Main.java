4dba8512dae9a112dc04a75ba582caf4ac2062a7
Thomas 'Noit' A <tba@live.dk>
2022-07-25 13:08:00 +0200

    Feat: Add logger to Main add 2 log messages

--- automatic-timetable-generation/src/main/java/Main.java
+++ automatic-timetable-generation/src/main/java/Main.java
@@ -1,16 +1,22 @@
 import models.ScheduleItemInfo;
 import models.Timetable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import services.HardcodedData;
 import services.TimetableGenerator;
 
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String... args){
         TimetableGenerator generator = new TimetableGenerator();
 
         Timetable timetable = generator.generateTimetable(HardcodedData.CreateSchool(), HardcodedData.CreateSetOfTeachers(), HardcodedData.CreateCurriculum());
 
+        logger.info("Printing timetable entries");
         for (ScheduleItemInfo entry : timetable.getItinerary()) {
             System.out.println(entry.getDuration());
         }
+        logger.info("Finished printing timetable entries");
     }
 }
