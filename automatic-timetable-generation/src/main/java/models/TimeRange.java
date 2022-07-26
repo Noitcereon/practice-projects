6e10a4ece9bc241742112ed75fd5505ca31ac86b
Thomas 'Noit' A <tba@live.dk>
2022-07-26 16:22:55 +0200

    Refactor: Move getDayOfWeek into TimeHelper

--- automatic-timetable-generation/src/main/java/models/TimeRange.java
+++ automatic-timetable-generation/src/main/java/models/TimeRange.java
@@ -1,9 +1,8 @@
 package models;
 
 import enums.CustomDayOfWeek;
-import services.TimeInMs;
+import services.TimeHelper;
 
-import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 
@@ -60,37 +59,16 @@ public class TimeRange {
         int hoursToRemoveFromWorkDay = 24 - hoursUsedPerDay;
         int totalHoursToRemove = 0;
         while(dateBetweenStartAndEnd.before(end)){
-            if(daysToUse.contains(getDayOfTheWeek(dateBetweenStartAndEnd))){
+            if(daysToUse.contains(TimeHelper.getDayOfTheWeek(dateBetweenStartAndEnd))){
                 totalHoursToRemove += hoursToRemoveFromWorkDay;
             }
             else{
                 totalHoursToRemove += 24;
             }
-            dateBetweenStartAndEnd.setTime(dateBetweenStartAndEnd.getTime() + TimeInMs.day());
+            dateBetweenStartAndEnd.setTime(dateBetweenStartAndEnd.getTime() + TimeHelper.day());
         }
         return totalHoursToRemove;
     }
 
-    /**
-     *
-     * @param date A date.
-     * @return The day of the week, the date holds.
-     */
-    private CustomDayOfWeek getDayOfTheWeek(Date date){
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        int calendarDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-        // The Calendar class's DAY_OF_WEEK starts on a Sunday. Thus, Sunday = 1, Monday = 2 etc.
-        switch (calendarDayOfWeek){
-            case 1: return CustomDayOfWeek.SUNDAY;
-            case 2: return CustomDayOfWeek.MONDAY;
-            case 3: return CustomDayOfWeek.TUESDAY;
-            case 4: return CustomDayOfWeek.WEDNESDAY;
-            case 5: return CustomDayOfWeek.THURSDAY;
-            case 6: return CustomDayOfWeek.FRIDAY;
-            case 7: return CustomDayOfWeek.SATURDAY;
-            default:
-                throw new IllegalArgumentException("Date does not have a valid DAY_OF_WEEK");
-        }
-    }
+
 }
