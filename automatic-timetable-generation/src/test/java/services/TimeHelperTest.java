c7c1009e2f14012971bfed6408a20381249209f2
Thomas 'Noit' A <tba@live.dk>
2022-07-27 11:45:35 +0200

    Test(TimeHelper): Add success scenario tests

--- automatic-timetable-generation/src/test/java/services/TimeHelperTest.java
+++ automatic-timetable-generation/src/test/java/services/TimeHelperTest.java
@@ -1,12 +1,17 @@
 package services;
 
+import enums.CustomDayOfWeek;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TimeHelperTest {
 
+    private final Calendar calendar = Calendar.getInstance();
     @Test
     void dayInMs_IsCorrectMs() {
         long actual = TimeHelper.dayInMs();
@@ -29,42 +34,89 @@ class TimeHelperTest {
     }
 
     @Test
-    void getDayOfTheWeek() {
-        Assertions.fail();
+    void givenTheFirstDayOf2022_whenUsingTimeHelperToGetDayOfWeek_thenReturnSaturday() {
+        Date date = TimeHelper.createDate(2022, 1, 1, 0, 0);
+        CustomDayOfWeek expected = CustomDayOfWeek.SATURDAY;
+        CustomDayOfWeek actual = TimeHelper.getDayOfTheWeek(date);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void createDate() {
-        Assertions.fail();
+    void givenAYearMonthHourAndMinute_whenCreatingDateWithTimeHelper_thenADateIsCreatedWithGivenValues() {
+        int expectedYear = 2000;
+        int expectedMonth = 1;
+        int expectedDayOfMonth = 24;
+        int expectedHourOfDay = 9;
+        int expectedMinute = 3;
+        Date date = TimeHelper.createDate(expectedYear, expectedMonth, expectedDayOfMonth, expectedHourOfDay, expectedMinute);
+        calendar.setTime(date);
+        int actualMonth = calendar.get(Calendar.MONTH)+1; // +1 because Calendar months starts from 0.
+        Assertions.assertEquals(expectedYear, calendar.get(Calendar.YEAR));
+        Assertions.assertEquals(expectedMonth, actualMonth);
+        Assertions.assertEquals(expectedDayOfMonth, calendar.get(Calendar.DAY_OF_MONTH));
+        Assertions.assertEquals(expectedHourOfDay, calendar.get(Calendar.HOUR_OF_DAY));
+        Assertions.assertEquals(expectedMinute, calendar.get(Calendar.MINUTE));
+
     }
 
     @Test
-    void addYearsToDate() {
-        Assertions.fail();
+    void givenADate_whenUsingTimeHelperToAddYearToDate_thenItAddsOnYear() {
+        Date date = TimeHelper.createDate(2000, 12, 31, 22, 0);
+        int expectedYear = 2001;
+        Date dateOneYearLater = TimeHelper.addYearsToDate(date, 1);
+        calendar.setTime(dateOneYearLater);
+        int actualYear = calendar.get(Calendar.YEAR);
+        Assertions.assertEquals(expectedYear, actualYear);
     }
 
     @Test
-    void addMonthsToDate() {
-        Assertions.fail();
+    void givenADate_whenUsingTimeHelperToAddMonthToDate_thenItAddsOneMonth() {
+        Date date = TimeHelper.createDate(2000, 12, 31, 22, 0);
+        int expected = 0; // 0 = JANUARY in Calendar
+        Date dateOneMonthLater = TimeHelper.addMonthsToDate(date, 1);
+        calendar.setTime(dateOneMonthLater);
+        int actual = calendar.get(Calendar.MONTH);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void addDaysToDate() {
-        Assertions.fail();
+    void givenADate_whenUsingTimeHelperToAddDayToDate_thenItAddsOneDay() {
+        Date date = TimeHelper.createDate(2000, 12, 2, 22, 0);
+        int expected = 3;
+        Date dateOneDayLater = TimeHelper.addDaysToDate(date, 1);
+        calendar.setTime(dateOneDayLater);
+        int actual = calendar.get(Calendar.DAY_OF_MONTH);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void addHoursToDate() {
-        Assertions.fail();
+    void givenADate_whenUsingTimeHelperToAddHourToDate_thenItAddsOneHour() {
+        Date date = TimeHelper.createDate(2000, 12, 2, 16, 0);
+        int expected = 17;
+        Date dateOneHourLater = TimeHelper.addHoursToDate(date, 1);
+        calendar.setTime(dateOneHourLater);
+        int actual = calendar.get(Calendar.HOUR_OF_DAY);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void addMinutesToDate() {
-        Assertions.fail();
+    void givenADate_whenUsingTimeHelperToAddMinuteToDate_thenItAddsOneMinute() {
+        Date date = TimeHelper.createDate(2000, 12, 2, 16, 5);
+        int expected = 6;
+        Date dateOneMinuteLater = TimeHelper.addMinutesToDate(date, 1);
+        calendar.setTime(dateOneMinuteLater);
+        int actual = calendar.get(Calendar.MINUTE);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void addSecondsToDate() {
-        Assertions.fail();
+    void givenADate_whenUsingTimeHelperToAddSecondToDate_thenItAddsOneSecond() {
+        Date date = TimeHelper.createDate(2000, 12, 2, 16, 5);
+        calendar.setTime(date);
+        int expected = calendar.get(Calendar.SECOND)+1;
+        Date dateOneSecondLater = TimeHelper.addSecondsToDate(date, 1);
+        calendar.setTime(dateOneSecondLater);
+        int actual = calendar.get(Calendar.SECOND);
+        Assertions.assertEquals(expected, actual);
     }
 }
