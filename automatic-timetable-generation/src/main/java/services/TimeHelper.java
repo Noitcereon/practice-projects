eb87e246b3f7d047625c86265008537f87f54e30
Thomas 'Noit' A <tba@live.dk>
2022-07-27 08:50:53 +0200

    Feat(TimeHelper): Add way to add years, months etc. to a date

--- automatic-timetable-generation/src/main/java/services/TimeHelper.java
+++ automatic-timetable-generation/src/main/java/services/TimeHelper.java
@@ -76,4 +76,34 @@ public class TimeHelper {
         Date output = calendar.getTime();
         return output;
     }
+    public Date addYearsToDate(Date date, int years){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.YEAR, years);
+        return cal.getTime();
+    }
+    public Date addMonthsToDate(Date date, int months){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.MONTH, months);
+        return cal.getTime();
+    }
+    public Date addDaysToDate(Date date, int days){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DAY_OF_YEAR, days);
+        return cal.getTime();
+    }
+    public Date addMinutesToDate(Date date, int minutes){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.MINUTE, minutes);
+        return cal.getTime();
+    }
+    public Date addSecondsToDate(Date date, int seconds){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.SECOND, seconds);
+        return cal.getTime();
+    }
 }
