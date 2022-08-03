7028f431c7b2a8c474d38d6d3fde0540e7f605c4
Thomas 'Noit' A <tba@live.dk>
2022-08-03 15:13:58 +0200

    Add exceptions

---
+++ chest-sorting-algorithm-proof-of-concept/src/main/java/com/noitcereon/exceptions/NotImplementedException.java
@@ -0,0 +1,11 @@
+package com.noitcereon.exceptions;
+
+public class NotImplementedException extends RuntimeException{
+    public NotImplementedException(){
+        super();
+    }
+    public NotImplementedException(String message){
+        super(message);
+    }
+
+}
