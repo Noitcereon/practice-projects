6f541dda1bd74e5edb17231f4d72d164d2222800
Thomas 'Noit' A <tba@live.dk>
2022-08-04 11:51:17 +0200

    Add 2 new methods for creating chests

--- chest-sorting-algorithm-proof-of-concept/src/main/java/com/noitcereon/helpers/ChestFactory.java
+++ chest-sorting-algorithm-proof-of-concept/src/main/java/com/noitcereon/helpers/ChestFactory.java
@@ -2,6 +2,11 @@ package com.noitcereon.helpers;
 
 import com.noitcereon.exceptions.NotImplementedException;
 import com.noitcereon.minecraft.mock.Chest;
+import com.noitcereon.minecraft.mock.Item;
+import com.noitcereon.minecraft.mock.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChestFactory {
     public static Chest createSingleChestEmpty(){
@@ -17,6 +22,20 @@ public class ChestFactory {
     public static Chest createDoubleChestWithItems(){
         Chest chest = new Chest(true);
         return addMultipleItems(chest);
+    }
+    public static Chest createChest(boolean isDoubleChest, int itemStacksWithin, Item itemInStack, int amountInEachStack){
+        Chest chest = new Chest(isDoubleChest);
+
+        Map<Integer, ItemStack> inventory = new HashMap<>();
+        for(int i=1; i <= itemStacksWithin; i++){
+            ItemStack stack = ItemStackFactory.createItemStack(amountInEachStack, itemInStack);
+            inventory.put(i, stack);
+        }
+        chest.setInventoryMap(inventory);
+        return chest;
+    }
+    public static Chest createChest(boolean isDoubleChest, Map<Integer, ItemStack> chestInventory){
+        return new Chest(isDoubleChest, chestInventory);
     }
 
     private static Chest addMultipleItems(Chest chest) {
