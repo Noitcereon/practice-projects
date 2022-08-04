08d19e8c9b52d569289b65848e4e2c3c9a1f3718
Thomas 'Noit' A <tba@live.dk>
2022-08-04 10:03:29 +0200

    Add version 1 of sorting algorithm

--- chest-sorting-algorithm-proof-of-concept/src/main/java/com/noitcereon/sorting/ChestSorter.java
+++ chest-sorting-algorithm-proof-of-concept/src/main/java/com/noitcereon/sorting/ChestSorter.java
@@ -4,16 +4,28 @@ package com.noitcereon.sorting;
 import com.noitcereon.minecraft.mock.Chest;
 import com.noitcereon.minecraft.mock.ItemStack;
 
-import java.util.Comparator;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ChestSorter {
     public Chest sort(Chest chest) {
         Chest output = new Chest(chest.isDoubleChest(), chest.getInventoryMap());
 
+        Map<Integer, ItemStack> inventoryMap = output.getInventoryMap();
 
+        List<Map.Entry<Integer, ItemStack>> sortedEntries = inventoryMap.entrySet().stream().sorted((entry, entry2) -> {
+            int itemId = entry.getValue().getItem().getItemId();
+            int itemId2 = entry2.getValue().getItem().getItemId();
 
+            return Integer.compare(itemId, itemId2);
+        }).toList();
+        Map<Integer, ItemStack> sortedInventoryMap = new HashMap<>();
+        int nextSpot = 1;
+        for(var entry : sortedEntries){
+            sortedInventoryMap.putIfAbsent(nextSpot, entry.getValue());
+            nextSpot++;
+        }
+        output.setInventoryMap(sortedInventoryMap);
         return output;
     }
 }
