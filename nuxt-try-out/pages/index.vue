5b531062fa6c3bfcd5aa8df19810eb72193f048d
Thomas 'Noit' A <tba@live.dk>
2022-08-17 16:01:54 +0200

    Nuxt: remove dark styling

--- nuxt-try-out/pages/index.vue
+++ nuxt-try-out/pages/index.vue
@@ -3,18 +3,18 @@
 <template #default>
   <div>
     <h1
-      class="pb-4 text-6xl text-center text-gray-200 dark:text-gray-100 transition-colors"
+      class="pb-4 text-6xl text-center text-gray-800 transition-colors"
     >
       Home
     </h1>
-    <p class="text-gray-600 dark:text-pink-900">
+    <p class="text-gray-600">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde enim ipsum
       quo voluptates eos id ex saepe exercitationem necessitatibus? Dolore fuga
       quasi vel corporis illum, velit aspernatur est ex explicabo?
     </p>
     <section class="grid lg:grid-cols-3 w-full mt-6 gap-3">
       <div
-        class="flex justify-center bg-pink-200 dark:bg-opacity-20 rounded-md p-2"
+        class="flex justify-center bg-pink-200 rounded-md p-2"
       >
         <div>
           <h2 class="block text-3xl text-center">Column 1</h2>
@@ -27,7 +27,7 @@
         </div>
       </div>
       <div
-        class="flex justify-center bg-pink-300 dark:bg-opacity-20 rounded-md p-2"
+        class="flex justify-center bg-pink-300 rounded-md p-2"
       >
         <div>
           <h2 class="block text-3xl text-center">Colum 2</h2>
@@ -38,7 +38,7 @@
         </div>
       </div>
       <div
-        class="flex justify-center bg-pink-400 dark:bg-opacity-20 rounded-md p-2"
+        class="flex justify-center bg-pink-400 rounded-md p-2"
       >
         <div>
           <h2 class="block text-3xl text-center">Colum 3</h2>
