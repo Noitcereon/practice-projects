046a0bf94c27b3c09782a85b338fdda1210f5e74
Thomas 'Noit' A <tba@live.dk>
2022-08-17 15:29:03 +0200

    Nuxt: copied index from vue3-template

--- nuxt-try-out/pages/index.vue
+++ nuxt-try-out/pages/index.vue
@@ -1,8 +1,54 @@
 <script lang="ts" setup></script>
 
-<template>
+<template #default>
   <div>
-    Page: home
+    <h1
+      class="pb-4 text-6xl text-center text-gray-200 dark:text-gray-100 transition-colors"
+    >
+      Home
+    </h1>
+    <p class="text-gray-600 dark:text-pink-900">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde enim ipsum
+      quo voluptates eos id ex saepe exercitationem necessitatibus? Dolore fuga
+      quasi vel corporis illum, velit aspernatur est ex explicabo?
+    </p>
+    <section class="grid lg:grid-cols-3 w-full mt-6 gap-3">
+      <div
+        class="flex justify-center bg-pink-200 dark:bg-opacity-20 rounded-md p-2"
+      >
+        <div>
+          <h2 class="block text-3xl text-center">Column 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum natus
+            maxime officiis temporibus dolor itaque laboriosam architecto
+            consequuntur magni. Autem, natus repudiandae iste temporibus unde
+            itaque rem maxime ratione voluptas.
+          </p>
+        </div>
+      </div>
+      <div
+        class="flex justify-center bg-pink-300 dark:bg-opacity-20 rounded-md p-2"
+      >
+        <div>
+          <h2 class="block text-3xl text-center">Colum 2</h2>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus
+          dolorem optio aspernatur quidem tempora eaque reprehenderit, assumenda
+          non maxime dolores necessitatibus debitis corrupti eveniet minima
+          asperiores excepturi distinctio. Cum, laborum!
+        </div>
+      </div>
+      <div
+        class="flex justify-center bg-pink-400 dark:bg-opacity-20 rounded-md p-2"
+      >
+        <div>
+          <h2 class="block text-3xl text-center">Colum 3</h2>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora
+          perferendis cupiditate placeat fugit corrupti neque veniam iste
+          provident cumque facilis necessitatibus praesentium odit impedit alias
+          enim debitis, quas suscipit hic. lorem
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
