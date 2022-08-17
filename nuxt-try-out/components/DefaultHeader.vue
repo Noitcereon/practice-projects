0d8f57a56531f0f04b689fc3439055e5d1a731ff
Thomas 'Noit' A <tba@live.dk>
2022-08-17 16:09:23 +0200

    Nuxt: format DefaultHeader

--- nuxt-try-out/components/DefaultHeader.vue
+++ nuxt-try-out/components/DefaultHeader.vue
@@ -9,33 +9,39 @@ const props = defineProps({
 
 <template>
   <header class="absolute inset-x-0 top-0 lg:max-w-3xl lg:mx-auto">
-
-  <div class="container flex p-4 mx-auto">
-    <div class="container flex justify-start items-center lg:hidden">
-      <h1 class="text-2xl">{{ props.currentPage }}</h1>
+    <div class="container flex p-4 mx-auto">
+      <div class="container flex justify-start items-center lg:hidden">
+        <h1 class="text-2xl">{{ props.currentPage }}</h1>
+      </div>
+      <nav>
+        <ul>
+          <li
+            class="inline-block px-3 border-r-2 border-opacity-30 border-black"
+          >
+            <router-link to="/">Home</router-link>
+          </li>
+          <li
+            class="inline-block px-3 border-r-2 border-opacity-30 border-black"
+          >
+            <router-link to="about">Who am I</router-link>
+          </li>
+          <li
+            class="inline-block px-3 border-r-2 border-opacity-30 border-black"
+          >
+            <router-link to="prices">Prices</router-link>
+          </li>
+          <li
+            class="inline-block px-3 border-r-2 border-opacity-30 border-black"
+          >
+            <router-link to="contact">Contact</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="container flex justify-end flex-1">
+        <span>Menu button or something</span>
+      </div>
     </div>
-    <nav>
-      <ul>
-        <li class="inline-block px-3 border-r-2 border-opacity-30 border-black">
-          <router-link to="/">Home</router-link>
-        </li>
-        <li class="inline-block px-3 border-r-2 border-opacity-30 border-black">
-          <router-link to="about">Who am I</router-link>
-        </li>
-        <li class="inline-block px-3 border-r-2 border-opacity-30 border-black">
-          <router-link to="prices">Prices</router-link>
-        </li>
-        <li class="inline-block px-3 border-r-2 border-opacity-30 border-black">
-          <router-link to="contact">Contact</router-link>
-        </li>
-      </ul>
-    </nav>
-    <div class="container flex justify-end flex-1">
-      <span>Menu button or something</span>
-    </div>
-  </div>
   </header>
-
 </template>
 
 <style scoped></style>
