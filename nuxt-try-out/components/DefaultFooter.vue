00890580f888da20cbaba7fc8f3c680c16ca7be9
Thomas 'Noit' A <tba@live.dk>
2022-08-17 15:00:46 +0200

    Nuxt: Add default layout

---
+++ nuxt-try-out/components/DefaultFooter.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="w-full bg-black bg-opacity-20 p-4">
+    <div class="max-w-6xl mx-auto">
+      <ul class="list-none">
+        <h4 class="text-lg font-bold">Kontakt</h4>
+        <li>johndoe@mail.com</li>
+        <li>+45 00000000</li>
+        <li>Example address 42, 3000 CityName</li>
+      </ul>
+      <div class="mx-auto w-72 italic text-center text-xs">
+        <ul>
+          <li>Designed by Jane Doe</li>
+          <li>Developed by John Doe</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ nuxt-try-out/components/DefaultHeader.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+const props = defineProps({
+  currentPage: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="container flex p-4 mx-auto">
+    <div class="container flex justify-start items-center lg:hidden">
+      <h1 class="text-2xl">{{ props.currentPage }}</h1>
+    </div>
+    <nav>
+      <ul>
+        <li class="inline-block px-3 border-r-2 border-opacity-30 border-black">
+          <router-link to="/">Home</router-link>
+        </li>
+        <li class="inline-block px-3 border-r-2 border-opacity-30 border-black">
+          <router-link to="about">Who am I</router-link>
+        </li>
+        <li class="inline-block px-3 border-r-2 border-opacity-30 border-black">
+          <router-link to="prices">Prices</router-link>
+        </li>
+        <li class="inline-block px-3 border-r-2 border-opacity-30 border-black">
+          <router-link to="contact">Contact</router-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="container flex justify-end flex-1">
+      <span>Menu button or something</span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
