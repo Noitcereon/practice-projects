0de13e4c9f99b1482ba38a7af61182b82ccadc40
Thomas 'Noit' A <tba@live.dk>
2022-08-19 12:09:34 +0200

    Astro: Add Menulink

---
+++ astro-try-out/src/components/MenuLink.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  href: {
+    type: String,
+    required: true,
+  },
+  liClasses: {
+    type: String,
+    required: false,
+  },
+  linkClasses: {
+    type: String,
+    required: false,
+  },
+});
+</script>
+
+<template>
+  <li :class="props.liClasses" class="text-center py-1">
+    <a :href="props.href" :class="props.linkClasses" class="inline-block w-full">{{ props.text }}</a>
+  </li>
+</template>
