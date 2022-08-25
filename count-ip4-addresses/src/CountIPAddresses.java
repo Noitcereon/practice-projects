3393f59c86a7cdc48f8afe0018842b5b062bac04
Thomas 'Noit' A <tba@live.dk>
2022-08-25 14:57:09 +0200

    CountIPAddresses: add solution

--- count-ip4-addresses/src/CountIPAddresses.java
+++ count-ip4-addresses/src/CountIPAddresses.java
@@ -10,9 +10,22 @@ public class CountIPAddresses {
     // * With input "10.0.0.0", "10.0.1.0"   => return  256
     // * With input "20.0.0.10", "20.0.1.0"  => return  246
 
+
+    // I gave up after trying for approximately 2 hours and looked at solutions.
+    // This below solution is very similar to the top voted solution.
     public static long ipsBetween(String start, String end) {
-        int[] startIpSegments = Arrays.stream(start.split("\\.")).mapToInt(Integer::parseInt).toArray();
-        System.out.println(startIpSegments);
-        return -1;
+        long startIpAsLong = convertIp4ToLong(start);
+        long endIpAsLong = convertIp4ToLong(end);
+
+        return endIpAsLong-startIpAsLong;
+    }
+    public static long convertIp4ToLong(String ip4){
+        long[] ipSegments = Arrays.stream(ip4.split("\\.")).mapToLong(Long::parseLong).toArray();
+        long accumulator = 0;
+        for (long ipSegment : ipSegments) {
+            accumulator = (accumulator * 256) + ipSegment;
+        }
+        accumulator+=1;
+        return accumulator;
     }
 }
