eee689c148c656f027ccd193c43bcb00f41bd1da
Thomas 'Noit' A <tba@live.dk>
2022-08-26 08:39:38 +0200

    Add movie-api-spring-boot (henceforth movie-spring)
    movie-spring is an alias I will use to reference when adding code to this
    project.

---
+++ movie-api-spring-boot/src/test/java/com/noitcereon/movieapispringboot/MovieApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noitcereon.movieapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
