f3bf1e3951c45f95776ca74106010b9d5fc964b1
Thomas 'Noit' A <tba@live.dk>
2022-08-29 08:46:54 +0200

    movie-spring: Re-enable security
    Since I figured out why the config didn't register, I can enable it
    again with configurations in place.

--- movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/config/SecurityConfig.java
+++ movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/config/SecurityConfig.java
@@ -4,32 +4,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//    private final Logger logger = LoggerFactory.getLogger(getClass());
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        logger.info("Configuring security");
-//        http
-//                .cors().and()
-//
-//                .formLogin().disable()
-//
-//                // Sessions will not be used
-//                .sessionManagement().disable()
-//
-//                // Cross request forgery not required when no sessions (I guess because it's not a UI you access?)
-//                .csrf().disable()
-//
-//                .authorizeHttpRequests((authorization) -> authorization
-//                        .anyRequest().permitAll()
-//                )
-//                .httpBasic().disable();
-//
-//        return http.build();
-//    }
-//}
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        logger.info("Configuring security");
+        http
+                .cors().and()
+
+                .formLogin().disable()
+
+                // Sessions will not be used
+                .sessionManagement().disable()
+
+                // Cross request forgery not required when no sessions (I guess because it's not a UI you access?)
+                .csrf().disable()
+
+                .authorizeHttpRequests((authorization) -> authorization
+                        .anyRequest().permitAll()
+                )
+                .httpBasic().disable();
+
+        return http.build();
+    }
+}
