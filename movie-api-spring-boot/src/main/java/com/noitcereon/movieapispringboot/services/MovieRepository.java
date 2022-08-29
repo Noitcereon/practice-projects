f3371e4091a9046f178ae1ee09b5df26f1a032b1
Thomas 'Noit' A <tba@live.dk>
2022-08-29 11:58:34 +0200

    movie-spring: inject dataSource into movieRepository via Spring

--- movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/services/MovieRepository.java
+++ movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/services/MovieRepository.java
@@ -2,12 +2,29 @@ package com.noitcereon.movieapispringboot.services;
 
 import com.noitcereon.movieapispringboot.models.MovieCreate;
 import com.noitcereon.movieapispringboot.models.MovieEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
-public class MovieRepository implements ICrudRepository<MovieEntity, Long,  MovieCreate> {
+public class MovieRepository implements ICrudRepository<MovieEntity, Long, MovieCreate> {
+
+    private final Logger logger = LoggerFactory.getLogger(MovieRepository.class);
+    private final Connection connection;
+
+    public MovieRepository(DataSource dataSource) {
+        try {
+            this.connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public MovieEntity create(MovieCreate model) {
         return null;
