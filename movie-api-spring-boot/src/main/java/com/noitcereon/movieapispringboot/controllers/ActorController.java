6f1f327cae95dc5e57041a124997bd81a69a16ab
Thomas 'Noit' A <tba@live.dk>
2022-08-29 08:34:00 +0200

    movie-spring: Inject repos into their respective controllers

--- movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/controllers/ActorController.java
+++ movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/controllers/ActorController.java
@@ -1,8 +1,12 @@
 package com.noitcereon.movieapispringboot.controllers;
 
+import com.noitcereon.movieapispringboot.services.ActorRepository;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ActorController {
-
+    private final ActorRepository actorRepo;
+    public ActorController(ActorRepository actorRepo){
+        this.actorRepo = actorRepo;
+    }
 }
--- movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/controllers/MovieController.java
+++ movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/controllers/MovieController.java
@@ -1,5 +1,6 @@
 package com.noitcereon.movieapispringboot.controllers;
 
+import com.noitcereon.movieapispringboot.services.MovieRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,10 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/api/movie")
 public class MovieController {
-
+    private final MovieRepository movieRepo;
+    public MovieController(MovieRepository movieRepo){
+        this.movieRepo = movieRepo;
+    }
     @GetMapping
     public ResponseEntity<ArrayList<String>> getAll(){
         ArrayList<String> movies = new ArrayList<>();
