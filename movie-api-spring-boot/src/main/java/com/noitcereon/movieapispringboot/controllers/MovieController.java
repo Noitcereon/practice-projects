d958da55335412334ab38772b60e01a5c67bf76c
Thomas 'Noit' A <tba@live.dk>
2022-08-30 12:13:13 +0200

    movie-spring: Add more Swagger documentation

--- movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/controllers/MovieController.java
+++ movie-api-spring-boot/src/main/java/com/noitcereon/movieapispringboot/controllers/MovieController.java
@@ -4,6 +4,8 @@ import com.noitcereon.movieapispringboot.models.MovieCreate;
 import com.noitcereon.movieapispringboot.models.MovieEntity;
 import com.noitcereon.movieapispringboot.repositories.MovieRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +36,7 @@ public class MovieController implements ICrudController<MovieEntity, Long, Movie
     @Override
     @GetMapping("/{id}")
     @Operation(summary = "Retrieves a movie with all its data, including actors in the movie.")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200"), @ApiResponse(responseCode = "204")})
     public ResponseEntity<MovieEntity> getById(@PathVariable Long id) {
         MovieEntity movie = movieRepo.getById(id);
         if(movie == null){
@@ -46,15 +49,18 @@ public class MovieController implements ICrudController<MovieEntity, Long, Movie
     }
 
     @Override
+    @Operation(summary = "Updates a movie in the database based on the passed in MovieEntity")
     public ResponseEntity<MovieEntity> update(MovieEntity updatedModel) {
         return null;
     }
 
     @Override
+    @Operation(summary = "Adds a new movie to the database and returns the added movie")
     public ResponseEntity<MovieEntity> add(MovieCreate creationModel) {
         return null;
     }
 
+    @Operation(summary = "Deletes a movie from the database and returns the deleted movie.")
     @Override
     public ResponseEntity<MovieEntity> deleteById(Long aLong) {
         return null;
