403229622bb7d1c25c1581b739f989440ea62cf5
TAndersen24 <thomas.andersen@dxc.com>
2023-01-16 16:28:33 +0100

    Add a few system.out messages
    As documentation.

--- soap-learning/src/main/java/me/noitcereon/Main.java
+++ soap-learning/src/main/java/me/noitcereon/Main.java
@@ -2,6 +2,10 @@ package me.noitcereon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // https://www.baeldung.com/jax-ws
+        System.out.println("This is an introduction to SOAP Webservices.");
+        System.out.println("I am gonna try both a top-down approach (contract-first) and a bottom-up approach (code first).");
+        System.out.println("Top-down approach starts with a wsdl (Webservice Definition Language) file. See ./resources/schemas/");
+
     }
 }
