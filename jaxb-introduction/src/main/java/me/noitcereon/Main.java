4f7816219bcac653b0a6a4552556b84e8c18fb5e
TAndersen24 <thomas.andersen@dxc.com>
2023-01-19 09:36:41 +0100

    jaxb-introduction: Initial commit
    Just a new Maven project.

---
+++ jaxb-introduction/src/main/java/me/noitcereon/Main.java
@@ -0,0 +1,8 @@
+package me.noitcereon;
+
+public class Main {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
