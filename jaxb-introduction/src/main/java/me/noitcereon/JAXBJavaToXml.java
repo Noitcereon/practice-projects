df2947134b092b514382c9bb42fb25774a02bd26
TAndersen24 <thomas.andersen@dxc.com>
2023-01-19 15:25:20 +0100

    jaxb-introduction: Add convertPersonToXml and fix outdated string

--- jaxb-introduction/src/main/java/me/noitcereon/JAXBJavaToXml.java
+++ jaxb-introduction/src/main/java/me/noitcereon/JAXBJavaToXml.java
@@ -1,5 +1,6 @@
 package me.noitcereon;
 
+import me.noitcereon.exceptions.InternalApplicationException;
 import me.noitcereon.models.Person;
 
 import javax.xml.bind.JAXB;
@@ -8,6 +9,7 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 
 @SuppressWarnings("UseOfSystemOutOrSystemErr")
 public class JAXBJavaToXml {
@@ -17,7 +19,7 @@ public class JAXBJavaToXml {
 
     public static void convertPersonToXmlAndLogInfo(Person person) {
         try {
-            System.out.println("generatePersonAsXmlFile called with " + person);
+            System.out.println("convertPersonToXmlAndLogInfo called with " + person);
             File personAsFile = File.createTempFile("person", ".xml");
 
             JAXBContext jaxbContext = JAXBContext.newInstance(Person.class);
@@ -28,6 +30,23 @@ public class JAXBJavaToXml {
             System.out.println("\nFile path of person.xml: " + personAsFile.getAbsolutePath());
         } catch (IOException | JAXBException e) {
             System.err.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Converts a Person java object into XML and returns it.
+     * @param person The person to convert to XML.
+     * @return The given Person object as an XML string representation.
+     */
+    public static String convertPersonToXml(Person person) {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(Person.class);
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            StringWriter sw = new StringWriter();
+            marshaller.marshal(person, sw);
+            return sw.toString();
+        } catch (JAXBException e) {
+            throw new InternalApplicationException(e);
         }
     }
 
