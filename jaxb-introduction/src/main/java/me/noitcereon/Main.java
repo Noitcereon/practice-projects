01298d0ce87093c4e899d57a511e135729d485ba
TAndersen24 <thomas.andersen@dxc.com>
2023-01-19 15:44:30 +0100

    jaxb-introduction: Update Main to call both JAXB classes
    To show behaviour of both marshalling and unmarshalling.

--- jaxb-introduction/src/main/java/me/noitcereon/Main.java
+++ jaxb-introduction/src/main/java/me/noitcereon/Main.java
@@ -7,7 +7,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Person person = new Person("Thomas", "Andersen", 1998);
+        String personXml = JAXBJavaToXml.convertPersonToXml(person);
 
-        JAXBJavaToXml.convertPersonToXmlAndLogInfo(person);
+        Person xmlPersonAsJavaObject = JAXBXmlToJava.convertXmlToPerson(personXml);
+        System.out.println(xmlPersonAsJavaObject);
     }
 }
