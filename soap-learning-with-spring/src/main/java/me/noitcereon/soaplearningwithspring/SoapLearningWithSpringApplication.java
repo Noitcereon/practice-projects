270a9e8d368ee6c03341f15f6fe52f505d8808e0
Thomas 'Noit' Andersen <thomas.andersen@dxc.com>
2023-02-06 13:38:29 +0100

    soap-learning-spring: Add startup logging

--- soap-learning-with-spring/src/main/java/me/noitcereon/soaplearningwithspring/SoapLearningWithSpringApplication.java
+++ soap-learning-with-spring/src/main/java/me/noitcereon/soaplearningwithspring/SoapLearningWithSpringApplication.java
@@ -1,12 +1,24 @@
 package me.noitcereon.soaplearningwithspring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SoapLearningWithSpringApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SoapLearningWithSpringApplication.class);
     public static void main(String[] args) {
+        if(args.length != 0){
+            LOG.info("Starting application with the following arguments: ");
+            for (String arg : args){
+                LOG.info("Argument: {}", arg);
+            }
+        }
+        else {
+            LOG.debug("Application starting without additional arguments set.");
+        }
         SpringApplication.run(SoapLearningWithSpringApplication.class, args);
     }
 
