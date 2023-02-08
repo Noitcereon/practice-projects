79a3e76123628df6762f10de6f814a60baea8dc9
Thomas 'Noit' A <tba@live.dk>
2023-02-08 11:17:45 +0100

    soap-learning-spring: Update WebServiceConfig with calculator

--- soap-learning-with-spring/src/main/java/me/noitcereon/soaplearningwithspring/config/WebServiceConfig.java
+++ soap-learning-with-spring/src/main/java/me/noitcereon/soaplearningwithspring/config/WebServiceConfig.java
@@ -8,6 +8,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.WsdlDefinition;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
@@ -24,7 +25,7 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     }
 
     @Bean(name = "countries")
-    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema){
+    public WsdlDefinition countriesWsdlDefinition(XsdSchema countriesSchema){
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("CountriesPort");
         wsdl11Definition.setLocationUri("/ws");
@@ -32,8 +33,21 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         wsdl11Definition.setSchema(countriesSchema);
         return wsdl11Definition;
     }
+    @Bean(name = "calculator")
+    public WsdlDefinition calculatorWsdlDefinition(XsdSchema calculatorSchema){
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("CalculatorPort");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://noitcereon.com/calculator");
+        wsdl11Definition.setSchema(calculatorSchema);
+        return wsdl11Definition;
+    }
     @Bean
     public XsdSchema countriesSchema(){
         return new SimpleXsdSchema(new ClassPathResource("/schemas/countries.xsd"));
+    }
+    @Bean
+    public XsdSchema calculatorSchema(){
+        return new SimpleXsdSchema(new ClassPathResource("/schemas/calculator.xsd"));
     }
 }
