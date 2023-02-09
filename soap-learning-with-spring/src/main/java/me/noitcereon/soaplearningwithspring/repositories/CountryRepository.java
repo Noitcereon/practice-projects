d961db3ddfd72ca093b30ab5e667b8f2c49eefb3
Noitcereon <tba@live.dk>
2023-02-09 08:55:53 +0100

    soap-learning-spring: update CountryRepository interface for CrUD

--- soap-learning-with-spring/src/main/java/me/noitcereon/soaplearningwithspring/repositories/CountryRepository.java
+++ soap-learning-with-spring/src/main/java/me/noitcereon/soaplearningwithspring/repositories/CountryRepository.java
@@ -4,4 +4,15 @@ import io.spring.guides.gs_producing_web_service.Country;
 
 public interface CountryRepository {
     Country findCountry(String name);
+
+    Country createCountry(Country newCountry);
+
+    /**
+     *
+     * @param name Name of the country to update.
+     * @param updatedCountry A <code>Country</code> object with the new values.
+     * @return The changed country.
+     */
+    Country updateCountry(String name, Country updatedCountry);
+    String deleteCountry(String countryName);
 }
