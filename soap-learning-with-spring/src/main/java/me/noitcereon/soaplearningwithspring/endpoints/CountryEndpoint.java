7f52fd966d0e468fcd6641208d685290a63bb15a
Noitcereon <tba@live.dk>
2023-02-09 08:54:40 +0100

    soap-learning-spring: add createCountry endpoint

--- soap-learning-with-spring/src/main/java/me/noitcereon/soaplearningwithspring/endpoints/CountryEndpoint.java
+++ soap-learning-with-spring/src/main/java/me/noitcereon/soaplearningwithspring/endpoints/CountryEndpoint.java
@@ -1,6 +1,8 @@
 package me.noitcereon.soaplearningwithspring.endpoints;
 
 
+import io.spring.guides.gs_producing_web_service.CreateCountryRequest;
+import io.spring.guides.gs_producing_web_service.CreateCountryResponse;
 import io.spring.guides.gs_producing_web_service.GetCountryRequest;
 import io.spring.guides.gs_producing_web_service.GetCountryResponse;
 import me.noitcereon.soaplearningwithspring.repositories.CountryRepository;
@@ -29,4 +31,14 @@ public class CountryEndpoint {
 
         return response;
     }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "createCountryRequest")
+    @ResponsePayload
+    public CreateCountryResponse createCountry(@RequestPayload CreateCountryRequest request) {
+        CreateCountryResponse response = new CreateCountryResponse();
+        response.setCreatedCountry(countryRepository.createCountry(request.getCountry()));
+        return response;
+    }
+    // TODO: Make updateCountry
+    // TODO: Make deleteCountry
 }
