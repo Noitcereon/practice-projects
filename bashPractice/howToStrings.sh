66e91ac0763e391e156956d0522a39a17e7ec7b9
Noitcereon <tba@live.dk>
2025-01-03 22:48:04 +0100

    bashPractice: minor grammar adjustments

--- bashPractice/howToStrings.sh
+++ bashPractice/howToStrings.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/bash
 
 # declare variable. No spaces!
-greeting="Welcome"
+greeting="Welcome back"
 user=$(whoami)
 dateAndTime=$(date)
 
 # Simple use of a variable: $variableName
-echo "$greeting back $user! Today is $dateAndTime."
+echo "$greeting $user! Today is $dateAndTime."
 echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
 echo
 # Get the length of a String variable: ${#variableName}
 echo "greeting is ${#greeting} in length"
 echo "user is ${#user} in length"
 
-# If syntax. Spaces matter! (identation doesn't). If syntax ends with fi
+# If syntax. Spaces matter! (identation doesn't). 'If' syntax ends with fi
 if [ ${#user} == ${#user} ]; 
     then
         echo "They're the same!"
